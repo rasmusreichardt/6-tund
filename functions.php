8c2671db33dbe3496f963d842a13f8dcdadb4ce9
Rasmus Reichardt <rasmrei@greeny.cs.tlu.ee>
2015-10-05 14:38:23 +0300

    added delete

--- functions.php
+++ functions.php
@@ -6,8 +6,8 @@
     function getAllData(){
           
         $mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
-
-        $stmt = $mysqli->prepare("SELECT id, user_id, number_plate, color FROM car_plates");
+        // deleted IS NULL - ei ole kustutatud
+        $stmt = $mysqli->prepare("SELECT id, user_id, number_plate, color FROM car_plates WHERE deleted IS NULL");
         $stmt->bind_result($id_from_db, $user_id_from_db, $number_plate_from_db, $color_from_db);
         $stmt->execute();
 
@@ -24,6 +24,8 @@
             
             $car->id = $id_from_db;
             $car->number_plate = $number_plate_from_db; 
+            $car->user_id = $user_id_from_db; 
+            $car->color = $color_from_db; 
             
             //lisan massiivi (auto lisan massiivi)
             array_push($array, $car);
@@ -37,6 +39,23 @@
         
         $stmt->close();
         $mysqli->close();
+    }
+    
+    function deleteCarData($car_id){
+        
+        $mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+        
+        // uuendan välja deleted, lisan praeguse date'i
+        $stmt = $mysqli->prepare("UPDATE car_plates SET deleted=NOW() WHERE id=?");
+        $stmt->bind_param("i", $car_id);
+        $stmt->execute();
+        
+        // tühjendame aadressirea
+        header("Location: table.php");
+        
+        $stmt->close();
+        $mysqli->close();
+        
     }
     
     
--- table.php
+++ table.php
@@ -1,18 +1,43 @@
 <?php  
     require_once("functions.php");
     
+    
+    // kuulan, kas kasutaja tahab kustutada
+    // ?delete=... on aadressireal
+    if(isset($_GET["delete"])) {
+        ///saadan kustutatava auto id
+        deleteCarData($_GET["delete"]);
+    }
+    
+    
+    
     // kõik autod objektide kujul massiivis
     $car_array = getAllData();
 ?>
 
 <h1>Tabel</h1>
+<table border=1>
+<tr>
+    <th>id</th>
+    <th>kasutaja id</th>
+    <th>auto numbrimärk</th>
+    <th>värv</th>
+    <th></th>
+</tr>
 <?php 
     
     // autod ükshaaval läbi käia
     for($i = 0; $i < count($car_array); $i++){
+        echo "<tr>";
+        echo "<td>".$car_array[$i]->id."</td>";
+        echo "<td>".$car_array[$i]->user_id."</td>";
+        echo "<td>".$car_array[$i]->number_plate."</td>";
+        echo "<td>".$car_array[$i]->color."</td>";
+        echo "<td><a href='?delete=".$car_array[$i]->id."'>X</a></td>";
+        echo "</tr>";
         
-        echo $car_array[$i]->number_plate."<br>";
         
     }
     
 ?>
+</table>
